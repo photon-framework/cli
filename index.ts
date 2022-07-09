68eab22a30e82f1bed0770ec54035299d4ac42dc
Frank Mayer <frank.mayer1907@protonmail.com>
2022-07-09 21:20:06 +0200

    added coderaiser/try-to-catch

--- index.ts
+++ index.ts
@@ -4,45 +4,38 @@ import { exit, log, logLevel, options } from "./src/cli";
 import { createRobots } from "./src/createRobots";
 import { createSitemap } from "./src/createSitemap";
 import { prerender } from "./src/prerender";
+import tryToCatch from "try-to-catch";
 
-try {
-  (async () => {
-    // robots.txt
-    if (options["no-robots"]) {
-      log("Skipping robots.txt generation", logLevel.info);
-    } else {
-      const robotsTxtLocation = join(options.path, "robots.txt");
-      log("Generating robots.txt");
-      await writeFile(robotsTxtLocation, createRobots(), "utf8");
-    }
-
-    // sitemap.xml
-    if (options["no-sitemap"]) {
-      log("Skipping sitemap.xml generation", logLevel.info);
-    } else {
-      const sitemapXmlLocation = join(options.path, "sitemap.xml");
-      log("Generating sitemap.xml");
-      await writeFile(sitemapXmlLocation, createSitemap(), "utf8");
-    }
-
-    // prerender routes
-    log("Prerendering routes");
-    await prerender();
-  })()
-    .then(() => {
-      exit();
-    })
-    .catch((err) => {
-      if ((err as Error).message) {
-        exit(500, (err as Error).message);
-      } else {
-        exit(500, String(err));
-      }
-    });
-} catch (err) {
-  if ((err as Error).message) {
-    exit(500, (err as Error).message);
+tryToCatch(async () => {
+  // robots.txt
+  if (options["no-robots"]) {
+    log("Skipping robots.txt generation", logLevel.info);
   } else {
-    exit(500, String(err));
+    const robotsTxtLocation = join(options.path, "robots.txt");
+    log("Generating robots.txt");
+    await tryToCatch(writeFile, robotsTxtLocation, createRobots(), "utf8");
   }
-}
+
+  // sitemap.xml
+  if (options["no-sitemap"]) {
+    log("Skipping sitemap.xml generation", logLevel.info);
+  } else {
+    const sitemapXmlLocation = join(options.path, "sitemap.xml");
+    log("Generating sitemap.xml");
+    await tryToCatch(writeFile, sitemapXmlLocation, createSitemap(), "utf8");
+  }
+
+  // prerender routes
+  log("Prerendering routes");
+  await tryToCatch(prerender);
+}).then(([err]) => {
+  if (err) {
+    if ((err as Error).message) {
+      exit(500, (err as Error).message);
+    } else {
+      exit(500, String(err));
+    }
+  } else {
+    exit();
+  }
+});
--- src/prerender.ts
+++ src/prerender.ts
@@ -1,4 +1,4 @@
-import { log, logLevel, options } from "./cli";
+import { log, logLevel, options, exit } from "./cli";
 import { filesIn, fileToRoute } from "./tools";
 import { document } from "./dom";
 import { router } from "./router";
@@ -7,6 +7,7 @@ import { existsSync } from "fs";
 import { mkdir, readFile, writeFile } from "fs/promises";
 import Mustache from "mustache";
 import { view } from "./view";
+import tryToCatch from "try-to-catch";
 
 const refMap = new Map<string, string>();
 
@@ -16,7 +17,17 @@ export const prerender = async (): Promise<void> => {
     if (route === router.dataset.default || route !== router.dataset.fallback) {
       log(`Prerendering "${route}"`, logLevel.verbose);
 
-      router.innerHTML = await readFile(join(options.path, file), "utf8");
+      const [error, data] = await tryToCatch(
+        readFile,
+        join(options.path, file),
+        "utf8"
+      );
+      if (error) {
+        exit(500, error.message);
+      } else if (typeof data === "string") {
+        router.innerHTML = data;
+      }
+
       for (const script of Array.from(router.getElementsByTagName("script"))) {
         script.remove();
       }
@@ -33,28 +44,47 @@ export const prerender = async (): Promise<void> => {
           if (refMap.has(src)) {
             return refMap.get(src)!;
           } else {
-            const ref = await readFile(join(options.path, src), "utf8");
-            refMap.set(src, ref);
-            return ref;
+            const [error, ref] = await tryToCatch(
+              readFile,
+              join(options.path, src),
+              "utf8"
+            );
+
+            if (error) {
+              return exit(500, error.message);
+            } else {
+              refMap.set(src, ref as string);
+              return ref;
+            }
           }
         })();
 
-        el.outerHTML = Mustache.render(refHtml, view.plus(el.dataset));
+        el.outerHTML = Mustache.render(
+          refHtml as string,
+          view.plus(el.dataset)
+        );
       }
 
       const dir = join(options.path, route);
       if (!existsSync(dir)) {
         log(`Creating directory "${dir}"`, logLevel.verbose);
-        await mkdir(dir, {
+        const [error0] = await tryToCatch(mkdir, dir, {
           recursive: true,
         });
+        if (error0) {
+          exit(500, error0.message);
+        }
       }
 
-      await writeFile(
+      const [error1] = await tryToCatch(
+        writeFile,
         join(dir, "index.html"),
         document.documentElement.outerHTML,
         "utf8"
       );
+      if (error1) {
+        exit(500, error1.message);
+      }
     } else {
       log(`Skipping "${route}" for prerendering`, logLevel.verbose);
     }
