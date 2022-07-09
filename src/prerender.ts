bf2245347b98e638f3d82868293b338811352c29
Frank Mayer <frank.mayer1907@protonmail.com>
2022-07-09 20:53:54 +0200

    resolve references

--- src/prerender.ts
+++ src/prerender.ts
@@ -5,6 +5,10 @@ import { router } from "./router";
 import { join } from "path";
 import { existsSync } from "fs";
 import { mkdir, readFile, writeFile } from "fs/promises";
+import Mustache from "mustache";
+import { view } from "./view";
+
+const refMap = new Map<string, string>();
 
 export const prerender = async (): Promise<void> => {
   for (const file of filesIn("content")) {
@@ -15,6 +19,27 @@ export const prerender = async (): Promise<void> => {
       router.innerHTML = await readFile(join(options.path, file), "utf8");
       for (const script of Array.from(router.getElementsByTagName("script"))) {
         script.remove();
+      }
+
+      let el: HTMLElement | undefined;
+
+      while (
+        (el = document.querySelector("photon-ref[src]") as
+          | HTMLElement
+          | undefined)
+      ) {
+        const src = el.getAttribute("src")!;
+        const refHtml = await (async () => {
+          if (refMap.has(src)) {
+            return refMap.get(src)!;
+          } else {
+            const ref = await readFile(join(options.path, src), "utf8");
+            refMap.set(src, ref);
+            return ref;
+          }
+        })();
+
+        el.outerHTML = Mustache.render(refHtml, view.plus(el.dataset));
       }
 
       const dir = join(options.path, route);
